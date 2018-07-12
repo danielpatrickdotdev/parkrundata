9e1b8a36142be18261ab35a888eadcadbabbb8b4
Daniel Patrick <danielpatrick@hotmail.com>
2018-07-12 17:17:17 +0100

    Refactor some tests and stub out new tests for create, edit, delete views

--- tests/test_views.py
+++ tests/test_views.py
@@ -15,7 +15,7 @@ from rest_framework.test import APIRequestFactory, APITestCase
 from parkrundata import models, views
 
 
-class TestEventViewSet(APITestCase):
+class TestEventViewSetBase(APITestCase):
 
     def setUp(self):
         self.uk = models.Country.objects.create(
@@ -51,16 +51,24 @@ class TestEventViewSet(APITestCase):
 
         self.factory = APIRequestFactory()
 
-    def setup_view(self, request, args=None, kwargs=None):
+    def setup_view(self, request=None, url=None, args=None, kwargs=None):
         view = views.EventViewSet()
-        view.request = request
+        view.request = request or self.factory.get(url or "")
         view.args = args
         view.kwargs = kwargs
         return view
 
+    def tearDown(self):
+        pass
+
+
+class TestEventViewSetWithoutAuthentication(TestEventViewSetBase):
+    """
+    Tests that need to be executed by AnonymousUser
+    """
+
     def test_retrieve_event(self):
-        request = self.factory.get("")
-        view = self.setup_view(request, kwargs={"pk": 1})
+        view = self.setup_view(kwargs={"pk": 1})
         obj = view.get_object()
 
         self.assertEqual(obj, self.bushy)
@@ -70,18 +78,37 @@ class TestEventViewSet(APITestCase):
         self.assertEqual(response.accepted_media_type, "application/json")
 
     def test_retrieve_event_list(self):
-        request = self.factory.get("")
-        view = self.setup_view(request)
+        view = self.setup_view()
         events = view.get_queryset()
         self.assertCountEqual(events,
                               [self.bushy, self.lesdougnes, self.neckarau])
 
     def test_invalid_pk_raises_404(self):
         for pk in [0, 4]:
-            request = self.factory.get("")
-            view = self.setup_view(request, kwargs={"pk": pk})
+            view = self.setup_view(kwargs={"pk": pk})
             with self.assertRaises(Http404):
                 view.get_object()
 
-    def tearDown(self):
+    def test_cannot_create_event_unless_authenticated(self):
+        pass
+
+    def test_cannot_modify_event_unless_authenticated(self):
+        pass
+
+    def test_cannot_delete_event_unless_authenticated(self):
+        pass
+
+
+class TestEventViewSetWithAuthentication(TestEventViewSetBase):
+    """
+    Tests that need to be executed by authenticated User
+    """
+
+    def test_create_event(self):
+        pass
+
+    def test_modify_event(self):
+        pass
+
+    def test_delete_event(self):
         pass
